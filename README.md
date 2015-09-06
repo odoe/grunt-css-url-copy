ef33c79029968f5fbe7f986e0b8308affb9bff8a
rene rubalcava <odoe@odoe.net>
2015-09-05 19:29:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# grunt-css-url-copy
+
+> Copy url resources in css files to flattened folder structure.
+
+The purpose of this plugin is to use it with other css minifiers that may merge multiple css files that all reference relative paths to image and font resources.
+This plugin will copy all resources to a single location that the minified resources can access.
+
+This plugin will copy your css file to a new file and not modify the original.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-css-url-copy --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-css-url-copy');
+```
+
+## The "css_url_copy" task
+
+### Overview
+In your project's Gruntfile, add a section named `cssurlcopy` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  cssurlcopy: {
+    options: {
+      root: 'release',
+      dest: 'release/app.css'
+    },
+    src: [
+      'release/app/css/main.css'
+    ],
+  },
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/css-url-copy.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+  var BASE_URL_REGEX = 'url\\(["\']?([^"\'\\(\\)]+?)["\']?\\)[};,!\\s]';'"]"])"]'
+  var path = require('path');
+
+  function processNextUrl(fileContent, currentUrlIndex, urlArray, baseDir, isVerbose, finishCallback) {
+    if (++currentUrlIndex === urlArray.length) {
+      finishCallback();
+    } else {
+      processUrl(fileContent, currentUrlIndex, urlArray, baseDir, isVerbose, finishCallback);
+    }
+  }
+
+  function processUrl(fileContent, currentUrlIndex, urlArray, baseDir, isVerbose, finishCallback) {
+    var url = urlArray[currentUrlIndex];
+    var nextUrl = processNextUrl.bind(null, fileContent, currentUrlIndex, urlArray, baseDir, isVerbose, finishCallback);
+    try {
+      if (isVerbose) {
+        grunt.log.writeln('\n[ #' + (currentUrlIndex + 1) + '  ]');
+      }
+      var urlFullPath = path.resolve(baseDir + '/' + url.split('?').shift());
+      if (isVerbose) {
+      }
+      if (!grunt.file.exists(urlFullPath)) {
+        var missingUrlMessage = '"' + (isVerbose ? urlFullPath : url.split('?').shift()) + '" not found on disk';
+        grunt.log.warn(missingUrlMessage);
+        return nextUrl();
+      }
+      var resourceName = url.replace(/\.\.\//g, '');
+      var queries = '';
+      if (resourceName.indexOf('?') > -1) {
+        var tmp = resourceName.split('?');
+        resourceName = tmp.shift();
+        var q = tmp.pop();
+        queries = '?' + q;
+      }
+      var escapedUrl = url.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+      var copyUrlRegex = '\\([\'"]?' + escapedUrl + '[\'"]?\\)';'"]"]'
+      fileContent.content = fileContent.content.replace(new RegExp(copyUrlRegex, 'g'), '("resources/' + resourceName + queries + '")');
+      return nextUrl();
+    } catch (e) {
+      grunt.log.error(e);
+      grunt.fail.warn('Failed to write "' + url + '"\n');
+    }
+  }
+
+  function processFile(fileSrc, fileDest, root, callback) {
+    try {
+      grunt.log.subhead('Processing source file "' + fileSrc + '"');
+      var fileContent = grunt.file.read(fileSrc);
+      var isVerbose = grunt.option('verbose');
+      var baseDir = path.resolve(path.dirname(fileSrc));
+      //var baseDir = path.resolve(process.cwd());
+      var resourceDir = process.cwd() + '/' + root + 'resources';
+      
+      var urlRegex = new RegExp(BASE_URL_REGEX, 'g');
+      var allUrls = [];
+      var urlMatch;
+      grunt.file.mkdir(resourceDir);
+      while ((urlMatch = urlRegex.exec(fileContent))) {
+        allUrls.push(urlMatch[1]);
+      }
+      var validUrls = allUrls.filter(function(url) { return (url.indexOf('data:') < 0) && (url.indexOf('//') < 0); });
+      if (validUrls.length === 0) {
+        grunt.log.writeln('Nothing to copy here!');
+        grunt.log.writeln('File "' + fileDest + '" not created');
+        return callback();
+      }
+      if (isVerbose) {
+        grunt.log.writeln('Using "' + resourceDir + '" as base directory for URLs');
+      }
+      var uniqueResourceUrls = grunt.util._.uniq(validUrls);
+      grunt.log.writeln(uniqueResourceUrls.length + ' resource URL' + (uniqueResourceUrls.length > 1 ? 's' : '') + ' found');
+      var fileContentRef = { content: fileContent  };
+      processUrl(fileContentRef, 0, uniqueResourceUrls, baseDir, isVerbose, function() {
+        grunt.file.write(fileDest, fileContentRef.content);
+        grunt.log.writeln('Done: File "' + fileDest + '" created');
+        callback();
+      });
+    } catch (e) {
+      grunt.log.error(e);
+      grunt.fail.warn('URL resource copy failed\n');
+    }
+  }
+
+  grunt.registerMultiTask('cssurlcopy', 'Copy URL resources to flattened folder structure.', function() {
+    var async = this.async();
+    grunt.log.writeln('File json "' + JSON.stringify(this.files) + '"');
+    var existingFiles = this.files.filter(function(file) {
+      if (!grunt.file.exists(file.src[0])) {
+        return false;
+      }
+      return true;
+    });
+
+    var leftToProcess = existingFiles.length;
+    if (leftToProcess === 0) {
+      async();
+    }
+    var options = this.options({});
+    existingFiles.forEach(function(file) {
+      processFile(file.src, options.dest, options.root + '/', function() {
+        if (--leftToProcess === 0) {
+          async();
+        }
+      });
+    });
+
+  });
+};
